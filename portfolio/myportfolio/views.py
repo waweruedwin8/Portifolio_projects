68c6e994cce9a1865a29aa89b7c597437669c07f
waweruedwin8 <waweruedwin8@gmail.com>
2024-07-01 16:31:43 +0300

    website landing pages

---
+++ portfolio/myportfolio/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from .models import Project
+
+def home(request):
+    projects = Project.objects.all()
+    return render(request, 'myportfolio/home.html', {'projects': projects})
